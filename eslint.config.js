8ac7efff3e069ddecc95f75b32d90cddfdb0fcf8
Binoy <binoypatel14@gmail.com>
2024-04-30 22:42:32 -0400

    chore(deps): upgrade to eslint@9

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+// @ts-check
+
+import js from '@eslint/js';
+import tsLint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: [
+      '.DS_Store',
+      '**/node_modules',
+      '**/build',
+      '**/.svelte-kit',
+      '**/package',
+      '.env',
+      '.env.*',
+      '!.env.example',
+
+      // Ignore files for PNPM, NPM and YARN
+      'pnpm-lock.yaml',
+      'package-lock.json',
+      'yarn.lock',
+    ],
+  },
+  js.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  eslintConfigPrettier,
+  ...eslintPluginSvelte.configs['flat/prettier'],
+  ...tsLint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2017,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-constant-binary-expression': 'off',
+    },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsLint.parser,
+        extraFileExtensions: ['.svelte'],
+      },
+    },
+  },
+];
